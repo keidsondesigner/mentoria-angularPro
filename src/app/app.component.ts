036438bd157fafe5c5f0e4017935039029f36044
Keidson Roby <keidsondesigner@gmail.com>
2024-07-12 12:43:41 -0400

    chore: Add ng-content ao Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HeaderComponent } from 'modules/feature/src/lib/header/header.component';
+import { ProductSearchComponent } from '@ecommerce/product-search';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, HeaderComponent],
+  imports: [
+    NxWelcomeComponent, 
+    RouterModule, 
+    HeaderComponent,
+    ProductSearchComponent
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
