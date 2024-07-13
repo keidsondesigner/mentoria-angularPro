601ce140e18d52f73190dae9a010059c7a85d079
Keidson Roby <keidsondesigner@gmail.com>
2024-07-13 05:11:27 -0400

    test: Criado teste HomeComponent

--- modules/feature/home/src/lib/home/home.component.spec.ts
+++ modules/feature/home/src/lib/home/home.component.spec.ts
@@ -1,13 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
+import { MatCardModule } from '@angular/material/card';
+import { RecommendedProductsService } from '@ecommerce/product-data-access';
+import { of } from 'rxjs';
+import { Product } from 'modules/data-access/product/src/lib/model/product.model';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
+  const mockProducts: Product[] = [
+    {
+      createdAt: "2023-10-10T03:25:53.791Z",
+      name: "Oriental Cotton Chicken",
+      price: "536.00",
+      description: "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016",
+      image: "https://loremflickr.com/640/480/nature",
+      id: "1",
+      quantity: 1
+    },
+    {
+      createdAt: "2023-10-09T20:45:06.372Z",
+      name: "Recycled Cotton Shoes",
+      price: "927.00",
+      description: "Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals",
+      image: "https://loremflickr.com/640/480/fashion",
+      id: "2",
+      quantity: 1
+    },
+  ];
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent],
+      imports: [HomeComponent, MatCardModule],
+      providers: [
+        {
+          provide: RecommendedProductsService,
+          useValue: { getProducts: () => of(mockProducts) }
+        }
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
@@ -17,5 +48,10 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render product cards correctly', () => {
+    const cards: HTMLElement[] = fixture.nativeElement.querySelectorAll('mat-card');
+    expect(cards.length).toBe(mockProducts.length);
   });
 });
