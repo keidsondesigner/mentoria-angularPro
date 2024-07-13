19349f59339fecf4b0629df814baf76d13b632ea
Keidson Roby <keidsondesigner@gmail.com>
2024-07-13 01:15:47 -0400

    feat: Criado Card de Produtos recomendados

--- modules/feature/home/src/lib/home/home.component.ts
+++ modules/feature/home/src/lib/home/home.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'lib-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  products = [
+    {
+      name: 'Camisa',
+      description: 'Camisa branca',
+      price: 19.99,
+      image: 'https://via.placeholder.com/150',
+    },
+  ];
+}
