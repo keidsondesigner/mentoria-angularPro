6d2bcfcb6ca6802a4fd1661e90ddbc6e732f28be
Keidson Roby <keidsondesigner@gmail.com>
2024-07-14 20:37:13 -0400

    style: atualizado style de UI  do ProductDetailComponent

--- modules/feature/product/detail/src/lib/product-detail/product-detail.component.ts
+++ modules/feature/product/detail/src/lib/product-detail/product-detail.component.ts
@@ -5,24 +5,29 @@ import { ActivatedRoute } from '@angular/router';
 import { map, Observable, switchMap } from 'rxjs';
 import { ProductSearchService } from '@ecommerce/product-data-access';
 import { Product } from 'modules/data-access/product/src/lib/model/product.model';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'lib-product-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss',
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+
+  product$!: Observable<Product>
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private productSearchService: ProductSearchService
   ) { }
 
-  product$: Observable<Product> = this.activatedRoute.params.pipe(
-    map(params => params['id']), // Pegando id da rota
-    switchMap(id => this.productSearchService.getById(id)) // Buscando pelo id
-  )
-
+  ngOnInit() {
+    this.product$ = this.activatedRoute.params.pipe(
+      map(params => params['id']), // Pegando id da rota
+      switchMap(id => this.productSearchService.getById(id)) // Buscando pelo id
+    )
+  }
 }
