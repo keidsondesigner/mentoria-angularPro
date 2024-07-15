ed4754bd0809327c08aa68dfcc1445fddb56c5f1
Keidson Roby <keidsondesigner@gmail.com>
2024-07-15 00:02:45 -0400

    feat: Criado Interceptor com Função

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from 'modules/feature/src/lib/header/header.component';
 import { ProductSearchComponent } from '@ecommerce/product-search';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   standalone: true,
   imports: [
-    RouterModule, 
+    RouterModule,
     HeaderComponent,
-    ProductSearchComponent
+    ProductSearchComponent,
+    MatSnackBarModule,
   ],
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { httpErrorsInterceptor } from './interceptors/http-errors/http-errors.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-    provideHttpClient(), provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([httpErrorsInterceptor])),  
+    provideAnimationsAsync(),
   ],
 };
