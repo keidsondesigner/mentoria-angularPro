4e6bb24a9b022c6ebdc3bf5b8e0caa2cc605e468
Keidson Roby <keidsondesigner@gmail.com>
2024-07-16 11:13:31 -0400

    test: Criado teste do inputEmailComponent

--- modules/feature/auth/form/src/lib/auth-form/auth-form-email/auth-form-email.component.spec.ts
+++ modules/feature/auth/form/src/lib/auth-form/auth-form-email/auth-form-email.component.spec.ts
@@ -1,4 +1,6 @@
 /* eslint-disable @nx/enforce-module-boundaries */
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AuthFormEmailComponent } from './auth-form-email.component';
 import { AuthFormComponent } from '@ecommerce/auth-form';
@@ -9,7 +11,7 @@ describe('AuthFormEmailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AuthFormEmailComponent],
+      imports: [AuthFormEmailComponent, RouterTestingModule, NoopAnimationsModule],
       providers: [AuthFormComponent],
     }).compileComponents();
 
@@ -20,5 +22,32 @@ describe('AuthFormEmailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should enable button when control is valid', () => {
+    const button: HTMLButtonElement = fixture.nativeElement.querySelector('button');
+    expect(button.disabled).toBe(true);
+
+    component.emailFormControl.setValue('email-valido@gmail.com');
+    fixture.detectChanges();
+    expect(button.disabled).toBe(false);
+  });
+
+  it('should display required error message', () => {
+    component.emailFormControl.setValue('');
+    component.emailFormControl.markAsTouched();  // campo foi tocado
+    fixture.detectChanges();
+
+    const errorRequired = fixture.nativeElement.querySelector('[data-testid="required-error"]');
+    expect(errorRequired).toBeTruthy();
+  });
+
+  it('should display email invalid error message', () => {
+    component.emailFormControl.setValue('email-invalido');
+    component.emailFormControl.markAsTouched(); // campo foi tocado
+    fixture.detectChanges();
+
+    const errorEmail = fixture.nativeElement.querySelector('[data-testid="email-error"]');
+    expect(errorEmail).toBeTruthy();
   });
 });
