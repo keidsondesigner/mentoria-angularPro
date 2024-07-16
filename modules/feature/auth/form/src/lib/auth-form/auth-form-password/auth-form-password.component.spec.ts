68ef3b1b965cf185bc179f0972fd76d7e9c2602b
Keidson Roby <keidsondesigner@gmail.com>
2024-07-16 11:32:02 -0400

    test: Criado teste do inputPasswordComponent

--- modules/feature/auth/form/src/lib/auth-form/auth-form-password/auth-form-password.component.spec.ts
+++ modules/feature/auth/form/src/lib/auth-form/auth-form-password/auth-form-password.component.spec.ts
@@ -1,5 +1,9 @@
+/* eslint-disable @nx/enforce-module-boundaries */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AuthFormPasswordComponent } from './auth-form-password.component';
+import { AuthFormComponent } from '@ecommerce/auth-form';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AuthFormPasswordComponent', () => {
   let component: AuthFormPasswordComponent;
@@ -7,7 +11,8 @@ describe('AuthFormPasswordComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AuthFormPasswordComponent],
+      imports: [AuthFormPasswordComponent, RouterTestingModule, NoopAnimationsModule],
+      providers: [AuthFormComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AuthFormPasswordComponent);
@@ -17,5 +22,32 @@ describe('AuthFormPasswordComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should enable button when control is valid', () => {
+    const button: HTMLButtonElement = fixture.nativeElement.querySelector('button');
+    expect(button.disabled).toBe(true);
+
+    component.passwordFormControl.setValue('123456');
+    fixture.detectChanges();
+    expect(button.disabled).toBe(false);
+  });
+
+  it('should display required error message', () => {
+    component.passwordFormControl.setValue('');
+    component.passwordFormControl.markAsTouched();  // campo foi tocado
+    fixture.detectChanges();
+
+    const errorRequired = fixture.nativeElement.querySelector('[data-testid="required-error"]');
+    expect(errorRequired).toBeTruthy();
+  });
+
+  it('should display password invalid error message', () => {
+    component.passwordFormControl.setValue('1234');
+    component.passwordFormControl.markAsTouched(); // campo foi tocado
+    fixture.detectChanges();
+
+    const errorPassword = fixture.nativeElement.querySelector('[data-testid="password-error"]');
+    expect(errorPassword).toBeTruthy();
   });
 });
