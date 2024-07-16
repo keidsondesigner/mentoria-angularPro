af524a96915babadfcaa9654b5ba23ade4edb4ee
Keidson Roby <keidsondesigner@gmail.com>
2024-07-16 09:57:21 -0400

    feat: Referenciando o Control do Pai AuthFormComponent, no component Filho AuthFormEmailComponent

--- modules/feature/auth/form/src/lib/auth-form/auth-form-email/auth-form-email.component.ts
+++ modules/feature/auth/form/src/lib/auth-form/auth-form-email/auth-form-email.component.ts
@@ -5,20 +5,25 @@ import { CommonModule } from '@angular/common';
 import { AuthFormComponent } from './../auth-form.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lib-auth-form-email',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatInputModule],
+  imports: [CommonModule, MatButtonModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './auth-form-email.component.html',
   styleUrl: './auth-form-email.component.scss',
 })
 export class AuthFormEmailComponent implements OnInit {
+
+  emailFormControl!: FormControl<string | null>;
   
   // AuthFormComponent Injeção da referência do componente pai
   constructor(private authFormComponent: AuthFormComponent) {}
   
   ngOnInit() {
     console.log(this.authFormComponent.form.value);
+
+    this.emailFormControl = this.authFormComponent.form.controls.email
   }
 }
