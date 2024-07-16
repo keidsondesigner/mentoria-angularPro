9a8ad2d3fc082f1d313a7aab1678b4c2fd5d0250
Keidson Roby <keidsondesigner@gmail.com>
2024-07-16 07:12:44 -0400

    feat: Criado authFormEmailComponent e authFormPasswordComponent para o formulário de múltiplas  etapas

---
+++ modules/feature/auth/form/src/lib/auth-form/auth-form-email/auth-form-email.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-auth-form-email',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './auth-form-email.component.html',
+  styleUrl: './auth-form-email.component.scss',
+})
+export class AuthFormEmailComponent {}
--- modules/feature/auth/form/src/lib/auth-form/auth-form.component.ts
+++ modules/feature/auth/form/src/lib/auth-form/auth-form.component.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @nx/enforce-module-boundaries */
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'lib-auth-form',
@@ -8,4 +10,11 @@ import { CommonModule } from '@angular/common';
   templateUrl: './auth-form.component.html',
   styleUrl: './auth-form.component.scss',
 })
-export class AuthFormComponent {}
+export class AuthFormComponent {
+  // FormBuilder é o mesmo que o FormGroup, mas com mais opções de validação e não preciso de um construtor;
+
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+}
