97952acbd4c6a83401df6deb15a7497e708777e4
Keidson Roby <keidsondesigner@gmail.com>
2024-07-16 10:01:27 -0400

    feat: Referenciando o Control do Pai AuthFormComponent, no component Filho AuthFormPasswordComponent

--- modules/feature/auth/form/src/lib/auth-form/auth-form-password/auth-form-password.component.ts
+++ modules/feature/auth/form/src/lib/auth-form/auth-form-password/auth-form-password.component.ts
@@ -1,14 +1,29 @@
+/* eslint-disable no-console */
 /* eslint-disable @nx/enforce-module-boundaries */
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { AuthFormComponent } from '../auth-form.component';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lib-auth-form-password',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatInputModule],
+  imports: [CommonModule, MatButtonModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './auth-form-password.component.html',
   styleUrl: './auth-form-password.component.scss',
 })
-export class AuthFormPasswordComponent {}
+export class AuthFormPasswordComponent {
+
+  passwordFormControl!: FormControl<string | null>;
+
+  // AuthFormComponent Injeção da referência do componente pai
+  constructor(private authFormComponent: AuthFormComponent) {}
+  
+  ngOnInit() {
+    console.log(this.authFormComponent.form.value);
+
+    this.passwordFormControl = this.authFormComponent.form.controls.email
+  }
+}
